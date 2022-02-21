5f2f8153d8122b7e4efb80f76ddbbcc34024e25b
Kyle Maiorana <kylemayo191@gmail.com>
2022-02-21 14:58:01 -0500

    create project2

---
+++ project2.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : project01.cpp
+// Author      : Alex Perez
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World";
+	return 0;
+}
